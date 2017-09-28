b1b95707c350223a6f3e24506a86eaa93b439e7f
helloxd <31766694+helloxdd@users.noreply.github.com>
2017-09-28 05:58:53 +0200

    Update Main.java

--- src/cc/helloxd/licensesystem/Main.java
+++ src/cc/helloxd/licensesystem/Main.java
@@ -1,52 +1,67 @@
 //@ https://github.com/ByteCode-DirectLeaks <3
 //@ https://github.com/ByteCode-DirectLeaks <3
-public String staticabusexdxd ("%%__USER__%%");
+//changed some thing to the original Hotspot code, didnt tested it out
+public boolean isblocked = false;
 
-private static void AntiPiracy() {
-         	try {
-         		String string;
-         		URLConnection uRLConnection = new URL("https://pastebin.com/raw/EXAMPLELINK").openConnection();
-         		uRLConnection.setConnectTimeout(3000);
-         		uRLConnection.setReadTimeout(3000);
-         		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(uRLConnection.getInputStream()));
-         		while ((string = bufferedReader.readLine()) != null) {
-             			if (!string.equals(staticabusexdxd)) continue;
-                 		Bukkit.getConsoleSender().sendMessage("§c§lYou are using a cracked plugin, disabling server and injecting backdoors");
-                 		Bukkit.getConsoleSender().sendMessage("§c§lYou are using a leaked plugin!");
- 				Bukkit.getConsoleSender().sendMessage("§c§lDisabling server!");
- 				Bukkit.getConsoleSender().sendMessage("§c§lUnpacking backdoors...");
- 				Bukkit.getConsoleSender().sendMessage("§c§l0% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l10% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l20% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l30% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l40% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l50% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l60% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l70% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l80% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l90% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l100% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§lFinised unpacking backdoors! Installing...");
- 				Bukkit.getConsoleSender().sendMessage("§c§l0% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l10% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l20% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l30% Done");
-            			Bukkit.getConsoleSender().sendMessage("§c§l40% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l50% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l60% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l70% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l80% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l90% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§l100% Done");
- 				Bukkit.getConsoleSender().sendMessage("§c§lBackdoors sucessfully installed! Crashing server...");
-                 		//maybe: Bukkit.getPluginManager.disablePlugin(this);
-				System.exit(0);
-				
-             		}
-            		return;
-         	} catch (Throwable t) {}
-    	}
- 
- }
+	
+	
 
-
+	public void AntiPiracy() {
+     	try {
+     		String string;
+     		URLConnection uRLConnection = new URL("https://pastebin.com/raw/EXAMPLELINK").openConnection();
+     		uRLConnection.setConnectTimeout(3000);
+     		uRLConnection.setReadTimeout(3000);
+     		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(uRLConnection.getInputStream()));
+     		while ((string = bufferedReader.readLine()) != null) {
+         			if (!string.contains("blocked")) return;
+         			else{
+         				
+                        Bukkit.getConsoleSender().sendMessage("§c§lYou are using a cracked plugin, disabling server and injecting backdoors");
+                  		Bukkit.getConsoleSender().sendMessage("§c§lYou are using a leaked plugin!");
+                        Bukkit.getConsoleSender().sendMessage("§c§lDisabling server!");
+          				Bukkit.getConsoleSender().sendMessage("§c§lUnpacking backdoors...");
+          				Bukkit.getConsoleSender().sendMessage("§c§l0% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l10% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l20% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l30% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l40% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l50% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l60% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l70% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l80% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l90% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l100% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§lFinised unpacking backdoors! Installing...");
+          				Bukkit.getConsoleSender().sendMessage("§c§l0% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l10% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l20% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l30% Done");
+                                 	Bukkit.getConsoleSender().sendMessage("§c§l40% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l50% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l60% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l70% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l80% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l90% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§l100% Done");
+          				Bukkit.getConsoleSender().sendMessage("§c§lBackdoors sucessfully installed! Crashing server...");
+          				isblocked = true;
+          				//maybe: Bukkit.getPluginManager().disablePlugin((Plugin) this);
+         				System.exit(0);
+         			}
+     		}
+     	}catch (IOException var6) {
+		    	  Bukkit.getPluginManager().disablePlugins();
+		    	  Bukkit.getConsoleSender().sendMessage("you dont have a connection to internet, please dont use this plugin in a localhost server / dont use an url blocker");
+		          var6.printStackTrace();
+	}
+}
+	
+	public void ontry(){
+		if (this.isblocked = true){
+			Bukkit.getPluginManager().disablePlugin((Plugin) this);
+			//or whatever else
+			return;
+		}
+	}
+}
